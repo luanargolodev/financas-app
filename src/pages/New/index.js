7c3ce54a332c81ea4fe143186056eb35f3b2e21b
Luan Argolo <ipsluan@icloud.com>
2022-10-09 23:40:44 -0300

    Registrar as receitas/despesas no firebase

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
-import { SafeAreaView, Keyboard, TouchableWithoutFeedback } from 'react-native';
+import {
+  SafeAreaView,
+  Keyboard,
+  TouchableWithoutFeedback,
+  Alert
+} from 'react-native';
 import {
   Background,
   Input,
@@ -8,11 +13,62 @@ import {
 } from './styles';
 import Header from '../../components/Header';
 import Picker from '../../components/Picker';
+import firebase from '../../services/firebaseConnection';
+import { useNavigation } from '@react-navigation/native';
 
 export default function New() {
+  const navigation = useNavigation();
   const [description, setDescription] = useState('');
   const [value, setValue] = useState('');
   const [type, setType] = useState('receita');
+
+  function handleSubmit() {
+    Keyboard.dismiss();
+
+    if(isNaN(parseFloat(value)) || type === null || description === '') {
+      alert('Preencha todos os campos!');
+      return;
+    }
+
+    Alert.alert(
+      'Confirmando dados',
+      `Você deseja adicionar uma ${type} de R$ ${value} com a descrição ${description}?`,
+      [
+        {
+          text: 'Cancelar',
+          style: 'cancel'
+        },
+        {
+          text: 'Continuar',
+          onPress: () => handleAdd()
+        }
+      ]
+    )
+  }
+
+  async function handleAdd() {
+    let uid = await firebase.auth().currentUser.uid;
+    let key = await firebase.database().ref('movements').child(uid).push().key;
+    await firebase.database().ref('movements').child(uid).child(key).set({
+      description: description,
+      value: parseFloat(value),
+      type: type,
+      date: new Date().toLocaleDateString()
+    });
+
+    let user = firebase.database().ref('users').child(uid);
+    await user.once('value').then((snapshot) => {
+      let balance = parseFloat(snapshot.val().balance);
+      type === 'despesa' ? balance -= parseFloat(value) : balance += parseFloat(value);
+      user.child('balance').set(balance);
+    });
+
+    Keyboard.dismiss();
+    setDescription('');
+    setValue('');
+    setType('receita');
+    navigation.navigate('Home');
+  }
 
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
@@ -28,7 +84,7 @@ export default function New() {
           />
 
           <Input
-            placeholder="Valor desejado"
+            placeholder="Valor"
             keyboardType="numeric"
             returnKeyType="next"
             onSubmitEditing={() => Keyboard.dismiss()}
@@ -38,7 +94,7 @@ export default function New() {
 
           <Picker onChange={setType} type={type} />
 
-          <SubmitButton>
+          <SubmitButton onPress={handleSubmit}>
             <SubmitText>Registrar</SubmitText>
           </SubmitButton>
         </SafeAreaView>
