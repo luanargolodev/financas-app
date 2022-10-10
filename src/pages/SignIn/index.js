946e5418095e561176c140e9c4deb083f0cfd7ac
Luan Argolo <ipsluan@icloud.com>
2022-10-10 18:26:41 -0300

    Corrigir erro no iOS e proteger senha do usuário

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -49,6 +49,7 @@ export default function SignIn() {
             autoCapitalize="none"
             value={password}
             onChangeText={(text) => setPassword(text)}
+            secureTextEntry={true}
           />
         </AreaInput>
 
