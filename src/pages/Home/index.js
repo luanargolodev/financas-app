638d5bc03ba488380fb06b58dff56da26c5bf96d
Luan Argolo <ipsluan@icloud.com>
2022-10-09 22:46:21 -0300

    Criar tela home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,18 +1,46 @@
-import React, { useContext } from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useContext, useState } from 'react';
+import {
+  Background,
+  Container,
+  Name,
+  Balance,
+  Title,
+  List
+} from './styles';
 import { AuthContext } from '../../contexts/auth';
+import Header from '../../components/Header';
+import MovementsList from '../../components/MovementsList';
 
-export default function Home() {  
-  const { user, signOut } = useContext(AuthContext);
+export default function Home() {
+  const [movements, setMovements] = useState([
+    {key: '1', type: 'receita', value: 1200},
+    {key: '2', type: 'despesa', value: 200},
+    {key: '3', type: 'receita', value: 40},
+    {key: '4', type: 'despesa', value: 89.62},
+    {key: '5', type: 'despesa', value: 89.62},
+    {key: '6', type: 'despesa', value: 89.62},
+    {key: '7', type: 'despesa', value: 89.62},
+    {key: '8', type: 'despesa', value: 89.62},
+    {key: '9', type: 'despesa', value: 89.62},
+  ]);
+  const { user } = useContext(AuthContext);
 
  return (
-    <View>
-      <Text>{user && user.name}</Text>
-      <Text>{user && user.email}</Text>
-      <Button
-        title="Sair"
-        onPress={() => signOut()}
+    <Background>
+      <Header />
+      <Container>
+        <Name>{user && user.name}</Name>
+        <Balance>R$ 123,00</Balance>
+      </Container>
+
+      <Title>Últimas movimentações</Title>
+
+      <List
+        showsVerticalScrollIndicator={false}
+        data={movements}
+        keyExtractor={item => item.key}
+        renderItem={({item}) => <MovementsList data={item} />}
       />
-    </View>
+    </Background>
   );
 }
